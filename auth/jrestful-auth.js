be4b0e57437171316a21798bb68c4ebbbbf0ea60
sp00m <christophe.maillard@rocketmail.com>
2015-07-30 17:26:15 +0200

    [1.0] jrestfulCore and jrestfulAuth modules started

---
+++ auth/jrestful-auth.js
@@ -0,0 +1,186 @@
+(function(angular) { 
+  
+  "use strict";
+  
+  /**
+   * Called in a configuration block, $injector belongs to the calling module.
+   */
+  var config = function($injector) {
+    // no-op
+  };
+  
+  /**
+   * Called in a run block, $injector belongs to the calling module.
+   */
+  var run = function($injector) {
+    
+    var Security = $injector.has("Security") ? $injector.get("Security") : {};
+    var $rootScope = $injector.get("$rootScope");
+    var Access = $injector.get("Access");
+    var $location = $injector.get("$location");
+    
+    if (!Security.auth || !Security.auth.unauthorizedPath || !Security.auth.forbiddenPath) {
+      throw "Security.auth must have following properties: unauthorizedPath, forbiddenPath";
+    }
+    
+    var routerErrorHandler = function(errorCode) {
+      if (rejection == Access.UNAUTHORIZED) {
+        $location.path(Security.auth.unauthorizedPath);
+      } else if (rejection == Access.FORBIDDEN) {
+        $location.path(Security.auth.forbiddenPath);
+      }
+    };
+    
+    $rootScope
+    .$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
+      routerErrorHandler(error);
+    })
+    .$on("$routeChangeError", function(event, current, previous, rejection) {
+      routerErrorHandler(rejection);
+    });
+    
+  };
+  
+  /**
+   * jrestfulAuth module, requires <code>jrestulCore</code>.
+   */
+  angular.module("jrestfulAuth", ["jrestfulCore"])
+  
+  /**
+   * Encapsulates the current user to implement the <code>hasRole</code>, <code>hasAnyRole</code>,
+   * <code>isAnonymous</code> and <code>isAuthenticated</code> methods.
+   */
+  .factory("UserProfile", ["$q", "User",
+  function($q, User) {
+  
+    var deferred = $q.defer();
+  
+    User.profile(function(userProfile) {
+      deferred.resolve({
+  
+        hasRole: function(role) {
+          return userProfile.roles.indexOf(role) >= 0;
+        },
+  
+        hasAnyRole: function(roles) {
+          return !!userProfile.roles.filter(function(role) {
+            return roles.indexOf(role) >= 0;
+          }).length;
+        },
+  
+        isAnonymous: function() {
+          return userProfile.anonymous;
+        },
+  
+        isAuthenticated: function() {
+          return !userProfile.anonymous;
+        }
+  
+      });
+    });
+  
+    return deferred.promise;
+  
+  }])
+  
+  /**
+   * Resolves or rejects a promise depending on the current user profile.
+   */
+  .factory("Access", ["$q", "UserProfile",
+  function($q, UserProfile) {
+  
+    var Access = {
+  
+      OK: 200,
+      UNAUTHORIZED: 401,
+      FORBIDDEN: 403,
+  
+      hasRole: function(role) {
+        var deferred = $q.defer();
+        UserProfile.then(function(userProfile) {
+          if (userProfile.hasRole(role)) {
+            deferred.resolve(Access.OK);
+          } else if (userProfile.isAnonymous()) {
+            deferred.reject(Access.UNAUTHORIZED);
+          } else {
+            deferred.reject(Access.FORBIDDEN);
+          }
+        });
+        return deferred.promise;
+      },
+  
+      hasAnyRole: function(roles) {
+        var deferred = $q.defer();
+        UserProfile.then(function(userProfile) {
+          if (userProfile.hasAnyRole(roles)) {
+            deferred.resolve(Access.OK);
+          } else if (userProfile.isAnonymous()) {
+            deferred.reject(Access.UNAUTHORIZED);
+          } else {
+            deferred.reject(Access.FORBIDDEN);
+          }
+        });
+        return deferred.promise;
+      },
+  
+      isAnonymous: function() {
+        var deferred = $q.defer();
+        UserProfile.then(function(userProfile) {
+          if (userProfile.isAnonymous()) {
+            deferred.resolve(Access.OK);
+          } else {
+            deferred.reject(Access.FORBIDDEN);
+          }
+        });
+        return deferred.promise;
+      },
+  
+      isAuthenticated: function() {
+        var deferred = $q.defer();
+        UserProfile.then(function(userProfile) {
+          if (userProfile.isAuthenticated()) {
+            deferred.resolve(Access.OK);
+          } else {
+            deferred.reject(Access.UNAUTHORIZED);
+          }
+        });
+        return deferred.promise;
+      }
+  
+    };
+  
+    return Access;
+  
+  }])
+
+  /**
+   * Initializer, extends <code>jrestfulProvider</code> <code>config</code> method to be called in a configuration block,
+   * and <code>jrestfulProvider</code> <code>run</code> method to be called in a run block.
+   */
+  .provider("jrestfulAuth", ["jrestfulProvider", function(jrestfulProvider) {
+    
+    var configCallfront = jrestfulProvider.config;    
+    jrestfulProvider.config = function($injector) {
+      configCallfront($injector);
+      config($injector);
+    };
+    
+    var runCallfront = jrestfulProvider.$get().run;
+    jrestfulProvider.$get = function() {
+      return {
+        run: function($injector) {
+          runCallfront($injector);
+          run($injector);
+        }
+      }
+    };
+    
+    return {
+      $get: function() {
+        throw "jrestfulAuth cannot be injected";
+      }
+    }
+    
+  }]);
+  
+})(angular);
--- core/jrestful-core.js
+++ core/jrestful-core.js
@@ -1,54 +1,101 @@
-angular.module("jrestful-core", ["ngResource"])
-
-.config(["$injector", "$httpProvider",
-function($injector, $httpProvider) {
-  
-  $httpProvider.defaults.headers.common["Accept"] = "application/hal+json";
-  
-  if ($injector.has("Application")) {
+(function(angular) { 
     
-    var Application = $injector.get("Application");
-    
-    $httpProvider.defaults.xsrfHeaderName = Application.CSRF.headerName;
-    $httpProvider.defaults.xsrfCookieName = Application.CSRF.cookieName;
-    
-    var uuid = function(e){ return e?(e^Math.random()*16>>e/4).toString(16):([1e7]+ -1e3+ -4e3+ -8e3+ -1e11).replace(/[018]/g,uuid); }
-    
-    $httpProvider.interceptors.push(function() {
-      return {
-        request: function(config) {
-          document.cookie = Application.CSRF.cookieName + "=" + uuid();
-          return config;
-        }
-      };
-    });
-    
-  }
-  
-}])
-
-.factory("RestResource", ["$resource",
-function($resource) {
-  
   "use strict";
   
-  return function(url, paramDefaults, actions, options) {
+  /**
+   * Called in a configuration block, $injector belongs to the calling module.
+   */
+  var config = function($injector) {
     
-    var extendedActions = {
-      query: { method: "GET" },
-      create: { method: "POST" },
-      update: { method: "PUT" }
-    };
+    var Security = $injector.has("Security") ? $injector.get("Security") : {};
+    var $httpProvider = $injector.get("$httpProvider");
     
-    var RestResource = $resource(url, paramDefaults, angular.extend(extendedActions, actions), options);
+    $httpProvider.defaults.headers.common["Accept"] = "application/hal+json";
     
-    RestResource.prototype.$save = function() {
-      var fn = this.id ? this.$update : this.$create;
-      return fn.apply(this, arguments);
-    };
-    
-    return RestResource;
+    if (Security.csrf) {
+      
+      if (!Security.csrf.headerName || !Security.csrf.cookieName) {
+        throw "Security.csrf must have following properties: headerName, cookieName";
+      }
+      
+      $httpProvider.defaults.xsrfHeaderName = Security.csrf.headerName;
+      $httpProvider.defaults.xsrfCookieName = Security.csrf.cookieName;
+      
+      var uuid = function(e){ return e?(e^Math.random()*16>>e/4).toString(16):([1e7]+ -1e3+ -4e3+ -8e3+ -1e11).replace(/[018]/g,uuid); }
+      
+      $httpProvider.interceptors.push(function() {
+        return {
+          request: function(config) {
+            document.cookie = Security.csrf.cookieName + "=" + uuid();
+            return config;
+          }
+        };
+      });
+      
+    }
     
   };
   
-}]);
+  /**
+   * Called in a run block, $injector belongs to the calling module.
+   */
+  var run = function($injector) {
+    // no-op
+  };
+  
+  /**
+   * jrestfulCore module, requires <code>ngResource</code>.
+   */
+  angular.module("jrestfulCore", ["ngResource"])
+  
+  /**
+   * Extends $resource:
+   * <ul>
+   * <li>The <code>query</code> method is not expecting an array anymore</li>
+   * <li>New <code>create</code> and <code>update</code> methods (respectively <code>POST</code> and <code>PUT</code>)</li>
+   * <li>The <code>save</code> method delegates to <code>create</code> or <code>update</code> methods,
+   *     depending on whether the resource has an <code>id</code> property.</li>
+   * </ul>
+   */
+  .factory("RestResource", ["$resource",
+  function($resource) {
+    
+    return function(url, paramDefaults, actions, options) {
+      
+      var extendedActions = {
+        query: { method: "GET" },
+        create: { method: "POST" },
+        update: { method: "PUT" }
+      };
+      
+      var RestResource = $resource(url, paramDefaults, angular.extend(extendedActions, actions), options);
+      
+      RestResource.prototype.$save = function() {
+        var fn = this.id ? this.$update : this.$create;
+        return fn.apply(this, arguments);
+      };
+      
+      return RestResource;
+      
+    };
+    
+  }])
+
+  /**
+   * Initializer, provides a <code>config</code> method to be called in a configuration block,
+   * and a <code>run</code> method to be called in a run block.
+   */
+  .provider("jrestful", [function() {
+    
+    return {
+      config: config,
+      $get: function() {
+        return {
+          run: run
+        }
+      }
+    }
+    
+  }]);
+  
+})(angular);
